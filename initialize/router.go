f6365d7698edbc1fbcc05b996cb64b4f903eae75
FlameMida <sweet.sakura@qq.com>
2021-10-27 14:18:50 +0800

    reformated

--- initialize/router.go
+++ initialize/router.go
@@ -19,13 +19,13 @@ func Routers() *gin.Engine {
 	Router.StaticFS(global.CONFIG.Local.Path, http.Dir(global.CONFIG.Local.Path)) // 为用户头像和文件提供静态地址
 	global.LOG.Info("use middleware logger")
 	// 跨域
-	//Router.Use(middleware.Cors()) // 如需跨域可以打开
+	// Router.Use(middleware.Cors())
 	global.LOG.Info("use middleware cors")
 	Router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	global.LOG.Info("register swagger handler")
 	// 方便统一添加路由组前缀 多服务器上线使用
 
-	//获取路由组实例
+	// 获取路由组实例
 	systemRouter := router.AppRouter.System
 	exampleRouter := router.AppRouter.Example
 	PublicGroup := Router.Group("")
